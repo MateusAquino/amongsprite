e42b7d58a2d9b1256db46582a4defb299d30c38e
MateusAquino <mateusaqb@gmail.com>
2020-11-14 09:04:29 -0300

    Fix test.js BG & package version

--- test.js
+++ test.js
@@ -3,5 +3,5 @@
     try { AmongSprite = require('amongsprite') } catch(err) { AmongSprite = require('./index') }
     const { Types } = AmongSprite;
 
-    x = await AmongSprite.create(575, '#D6C9BD', Types.BG["PORTAL SPACE"], null, Types.OUTFITS.RICK, null);
+    x = await AmongSprite.create(575, '#D6C9BD', Types.BG.PORTAL_SPACE, null, Types.OUTFITS.RICK, null);
 })();
